c59304852f37d375aa26dfac6ccebadbd106c162
redvrORtsma <161972959+redvrORtsma@users.noreply.github.com>
2026-01-17 06:05:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify 
 from flask_cors import CORS
 import requests
 import os
@@ -7,39 +7,19 @@ app = Flask(__name__)
 CORS(app)
 
 SITES = {
-    "GitHub": {
-        "url": "https://github.com/{}",
-        "not_found": "Not Found"
-    },
-    "Twitter": {
-        "url": "https://twitter.com/{}",
-        "not_found": "This account doesn’t exist"
-    },
-    "Reddit": {
-        "url": "https://www.reddit.com/user/{}",
-        "not_found": "nobody on Reddit goes by that name"
-    },
-    "Instagram": {
-        "url": "https://www.instagram.com/{}/",
-        "not_found": "Sorry, this page isn't available"
-    },
-    "TikTok": {
-        "url": "https://www.tiktok.com/@{}",
-        "not_found": "Couldn't find this account"
-    },
-    "YouTube": {
-        "url": "https://www.youtube.com/@{}",
-        "not_found": "This channel does not exist"
-    },
-    "Steam": {
-        "url": "https://steamcommunity.com/id/{}",
-        "not_found": "The specified profile could not be found"
-    },
-    "Twitch": {
-        "url": "https://www.twitch.tv/{}",
-        "not_found": "Sorry. Unless you’ve got a time machine"
-    }
+    "GitHub": {"url": "https://github.com/{}", "not_found": "Not Found"},
+    "Twitter": {"url": "https://twitter.com/{}", "not_found": "This account doesn’t exist"},
+    "Reddit": {"url": "https://www.reddit.com/user/{}", "not_found": "nobody on Reddit goes by that name"},
+    "Instagram": {"url": "https://www.instagram.com/{}/", "not_found": "Sorry, this page isn't available"},
+    "TikTok": {"url": "https://www.tiktok.com/@{}", "not_found": "Couldn't find this account"},
+    "YouTube": {"url": "https://www.youtube.com/@{}", "not_found": "This channel does not exist"},
+    "Steam": {"url": "https://steamcommunity.com/id/{}", "not_found": "The specified profile could not be found"},
+    "Twitch": {"url": "https://www.twitch.tv/{}", "not_found": "Sorry. Unless you’ve got a time machine"}
 }
+
+@app.route("/")
+def home():
+    return jsonify({"status": "ok", "message": "Username checker API is running"})
 
 @app.route("/check/<username>")
 def check(username):
